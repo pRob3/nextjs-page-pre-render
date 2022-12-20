0b88caa4fbb5a697f92b7abe9b09c26f0def2284
pRob3 <skene@hotmail.com>
2022-12-20 06:00:10 +0100

    Using getStaticPaths

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 import path from 'path';
-// import fs from 'fs';
+import Link from 'next/link';
 
 function HomePage(props) {
   const { products } = props;
@@ -7,7 +7,9 @@ function HomePage(props) {
   return (
     <ul>
       {products.map((product) => (
-        <li key={product.id}>{product.title}</li>
+        <li key={product.id}>
+          <Link href={`/${product.id}`}>{product.title}</Link>
+        </li>
       ))}
     </ul>
   );
