2b814409610018c78fed7ec2f409f6d5740c7a53
pRob3 <skene@hotmail.com>
2022-12-23 06:51:24 +0100

    Loading Paths Dynamically

--- pages/[pid]/index.js
+++ pages/[pid]/index.js
@@ -16,14 +16,20 @@ function ProductDetailsPage(props) {
   );
 }
 
-export async function getStaticProps(context) {
-  const { params } = context;
-  const productId = params.pid;
-
+async function getData() {
   const fs = require('fs').promises;
   const filePath = path.join(process.cwd(), 'data', 'dummy-backend.json');
   const jsonData = await fs.readFile(filePath);
   const data = JSON.parse(jsonData);
+
+  return data;
+}
+
+export async function getStaticProps(context) {
+  const { params } = context;
+  const productId = params.pid;
+
+  const data = await getData();
 
   const product = data.products.find((product) => product.id === productId);
 
@@ -35,9 +41,14 @@ export async function getStaticProps(context) {
 }
 
 export async function getStaticPaths() {
+  const data = await getData();
+
+  const ids = data.products.map((product) => product.id);
+  const pathsWithParams = ids.map((id) => ({ params: { pid: id } }));
+
   return {
-    paths: [{ params: { pid: 'p1' } }],
-    fallback: 'blocking', // true, false, blocking
+    paths: pathsWithParams,
+    fallback: false, // true, false, blocking
   };
 }
 
